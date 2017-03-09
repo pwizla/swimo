54973492e38da9120d1706342384d770cc2b1b6a
pwizla <pwizla+github@gmail.com>
2017-03-09 08:59:16 +0100

    Rewrite getTotal and getSubtotals + saveLocally on component update

--- src/App.js
+++ src/App.js
@@ -10,38 +10,53 @@ class App extends Component {
   constructor() {
     super();
     this.handleAddTransaction = this.handleAddTransaction.bind(this);
-    this.getTotalAmount = this.getTotalAmount.bind(this);
+    this.getTotal = this.getTotal.bind(this);
+    this.getSubtotals = this.getSubtotals.bind(this);
     this.state = {
       transactions: JSON.parse(localStorage.getItem('swimo-transactions')) || [],
-      total: JSON.parse(localStorage.getItem('swimo-total')) || 0
+      total: JSON.parse(localStorage.getItem('swimo-total')) || 0,
+      subtotals: JSON.parse(localStorage.getItem('swimo-subtotals')) || {
+        'Courses': 0,
+        'Factures': 0,
+        'Impôts': 0,
+        'Loyer': 0,
+        'Plaisir Perso': 0,
+        'Salaire': 0,
+        'Santé': 0,
+        'Sorties': 0,
+        'Transports': 0,
+        'Autres': 0
+      }
     };
   }
 
-  getTotalAmount () {
-    const transactions = this.state.transactions;
-    const amounts = [];
-    transactions.map( item => {
-      return amounts.push(Number(item.amount));
-    });
+  componentDidUpdate() {
+    this.saveLocally();
+  }
 
-    let newTotal = amounts.reduce( (a, b) => {
-      return a + b;
-    }, 0);
-
+  getTotal (amount) {
+    const newTotal = this.state.total + Number(amount);
     this.setState({total: newTotal});
-    this.saveLocally(newTotal, transactions);
+  }
+
+  getSubtotals (amount, category) {
+    const newSubtotals = this.state.subtotals;
+    newSubtotals[category] += Number(amount);
+    this.setState({subtotals: newSubtotals});
   }
 
   handleAddTransaction(obj) {
     let transactions = this.state.transactions;
     transactions.push(obj);
     this.setState({transactions: transactions});
-    this.getTotalAmount();
+    this.getTotal(obj.amount);
+    this.getSubtotals(obj.amount, obj.category);
   }
 
-  saveLocally(total, transactions) {
-    localStorage.setItem('swimo-transactions', JSON.stringify(transactions));
-    localStorage.setItem('swimo-total', JSON.stringify(total));
+  saveLocally() {
+    localStorage.setItem('swimo-transactions', JSON.stringify(this.state.transactions));
+    localStorage.setItem('swimo-total', JSON.stringify(this.state.total));
+    localStorage.setItem('swimo-subtotals', JSON.stringify(this.state.subtotals));
   }
 
   render() {
