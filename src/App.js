cce4957f3bdb7b974fd45dc036c065087f2c726d
pwizla <pwizla+github@gmail.com>
2017-03-13 15:47:07 +0100

    Move budget categories to settings file

--- src/App.js
+++ src/App.js
@@ -1,73 +1,26 @@
 import React, { Component } from 'react';
 import './css/app.css';
 import './css/react-bootstrap-table-all.min.css';
+import _ from 'lodash';
 import Header from './components/header.jsx';
 import NewTransaction from './components/new-transaction';
 import TransactionsList from './components/transactions-list';
 import Total from './components/total';
 import BudgetTable from './components/budget-table';
+import SETTINGS from './lib/settings';
 
 class App extends Component {
   constructor() {
     super();
     this.handleAddTransaction = this.handleAddTransaction.bind(this);
+    this.handleToggleChecked = this.handleToggleChecked.bind(this);
+    this.saveLocally = this.saveLocally.bind(this);
     this.getTotal = this.getTotal.bind(this);
     this.getBudget = this.getBudget.bind(this);
     this.state = {
       transactions: JSON.parse(localStorage.getItem('swimo-transactions')) || [],
       total: JSON.parse(localStorage.getItem('swimo-total')) || 0,
-      budget: JSON.parse(localStorage.getItem('swimo-budget')) || {
-        'Courses': {
-          'enveloppe': 50,
-          'engaged': 0,
-          'restant': 0,
-        },
-        'Factures': {
-          'enveloppe': 150,
-          'engaged': 0,
-          'restant': 0,
-        },
-        'Impôts': {
-          'enveloppe': 230,
-          'engaged': 0,
-          'restant': 0,
-        },
-        'Loyer': {
-          'enveloppe': 375,
-          'engaged': 0,
-          'restant': 0,
-        },
-        'Plaisir Perso': {
-          'enveloppe': 30,
-          'engaged': 0,
-          'restant': 0,
-        },
-        'Salaire': {
-          'enveloppe': 1500,
-          'engaged': 0,
-          'restant': 0,
-        },
-        'Santé': {
-          'enveloppe': 78,
-          'engaged': 0,
-          'restant': 0,
-        },
-        'Sorties': {
-          'enveloppe': 30,
-          'engaged': 0,
-          'restant': 0,
-        },
-        'Transports': {
-          'enveloppe': 80,
-          'engaged': 0,
-          'restant': 0,
-        },
-        'Autres': {
-          'enveloppe': 0,
-          'engaged': 0,
-          'restant': 0,
-        },
-      },
+      budget: JSON.parse(localStorage.getItem('swimo-budget')) || SETTINGS.budget,
       flatBudget: JSON.parse(localStorage.getItem('swimo-flatbudget')) || [],
     };
   }
